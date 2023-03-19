e6d0aba8e7f4853298f75c95e36fe692ec7c2ba5
ray_yokoyama <kingreibit@gmail.com>
2023-03-19 21:34:42 +0900

    extract switch process to func

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -15,15 +15,19 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "French":
 		prefix = frenchHelloPrefix
 	case "Japanese":
 		prefix = japaneseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
