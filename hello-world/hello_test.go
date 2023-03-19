05d6b30d5de9fedb5200a385a9a808d8badb7e46
ray_yokoyama <kingreibit@gmail.com>
2023-03-19 21:22:32 +0900

    add default value to Hello()

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  got := Hello("Ray")
-  want := "Hello, Ray"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
 
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
-  }
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Ray")
+		want := "Hello, Ray"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
